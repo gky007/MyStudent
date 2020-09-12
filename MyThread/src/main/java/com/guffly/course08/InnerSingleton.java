82d8676ce41d3b3aae25043e966f0df166c6c567
10844 <1084407178@qq.com>
2020-09-12 15:00:41 +0800

    线程回顾

---
+++ MyThread/src/main/java/com/guffly/course08/InnerSingleton.java
@@ -0,0 +1,34 @@
+package com.guffly.course08;
+
+public class InnerSingleton {
+    private  static class Singleton {	
+	private static Singleton single = new Singleton();
+    }
+    
+    public static Singleton getSingleton() {
+	return Singleton.single;
+    }
+    
+    public static void main(String[] args) {
+	Thread t1 = new Thread(new Runnable() {
+	    public void run() {
+		System.out.println(InnerSingleton.getSingleton().hashCode());
+	    }
+	}, "t1");
+	
+	Thread t2 = new Thread(new Runnable() {
+	    public void run() {
+		System.out.println(InnerSingleton.getSingleton().hashCode());
+	    }
+	}, "t2");
+	
+	Thread t3 = new Thread(new Runnable() {
+	    public void run() {
+		System.out.println(InnerSingleton.getSingleton().hashCode());
+	    }
+	}, "t3");
+	t1.start();
+	t2.start();
+	t3.start();
+    }
+}
